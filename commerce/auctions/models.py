552a8c867e6b9ff699f8e6292ee38c0373881f40
Duong Trong <maxplanck191@gmail.com>
2022-04-12 01:21:25 +0700

    commerce : create listing

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,28 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class AuctionListing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=300)
+    # time = models.TimeField()
+    # photo = models.ImageField()
+    def __str__(self):
+        return f"{self.title}"
+
+class Bid(models.Model):
+    startingBid = models.IntegerField()
+    currentPrice = models.IntegerField()
+    # time = models.TimeField()
+    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="bids")
+
+    def __str__(self):
+        return f"{self.listing.title}"
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=2000)
+    # time = models.TimeField()
+    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="comments")
+    
+
+
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import *
 
 
 def index(request):
@@ -61,3 +61,17 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+def create(request):
+    if request.method == "POST":
+        title = request.POST.get("title").strip()
+        description = request.POST.get("description").strip()
+        starting = request.POST.get("starting").strip()
+        listing = AuctionListing(title = request.POST.get("title").strip(),
+                                description = request.POST.get("description").strip())
+        listing.save()
+        bid = Bid(startingBid = request.POST.get("starting").strip(),
+                currentPrice = 0,
+                listing = listing)
+        bid.save()
+    return render(request, "auctions/create.html")
